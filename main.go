1f11f099899eba1feb50bbdb8af9e74bd490e67a
Rafael Luna Ribeiro <rafaelribeiro99@gmail.com>
2020-02-26 09:32:08 -0300

    Declarando structs

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+	//alex := person{"Alex", "Anderson"}
+	//alex := person{firstName: "Alex", lastName: "Anderson"}
+	//fmt.Println(alex)
+
+	var alex person
+
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+
+	fmt.Println(alex)
+	fmt.Printf("%+v", alex)
 }
