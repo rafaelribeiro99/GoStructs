854681d2f2e0c6ac3d2e8f7c8baeb9e1d54ed00a
Rafael Luna Ribeiro <rafaelribeiro99@gmail.com>
2020-02-27 21:06:55 -0300

    Commit final Structs

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 type contactInfo struct {
-	email    string
-	zipCoode int
+	email   string
+	zipCode int
 }
 
 type person struct {
@@ -29,13 +29,35 @@ func main() {
 		firstName: "Jim",
 		lastName:  "Party",
 		contactInfo: contactInfo{
-			email:    "jim@gmail.com",
-			zipCoode: 94000,
+			email:   "jim@gmail.com",
+			zipCode: 94000,
 		},
 	}
+	//jim.print()
 
+	//&jim esta atribuindo o ENDEREÇO DE MEMORIA de "jim"
+	//para jimPointer, assim podemos dar update no nome
+	//de Jim, mudando direitamente pelo endereço de memoria
+	//de "jim"
+	//jimPointer := &jim
+	//jimPointer.updateName("Jimmy")
+
+	//Atalho: como na func updateName tem o receiver *person
+	//e "jim" também é do tipo person, Go consegue interpretar
+	//que ambos são do mesmo tipo e que você quer o ENDEREÇO
+	//de jim, e não o seu valor, ja que ambos "jim" e "pointerToPerson"
+	//são do tipo... person
+	jim.updateName("Jimmy")
 	jim.print()
 
+}
+
+//o *person informa q queremos um endereço de memoria
+// chamado "pointerToPerson" do Type person
+func (pointerToPerson *person) updateName(newFirstName string) {
+	//Enquanto *pointerToPerson quer pegar o VALOR
+	//existente dentro do endereço de memoria "pointerToPerson"
+	(*pointerToPerson).firstName = newFirstName
 }
 
 func (p person) print() {
